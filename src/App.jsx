2fe8151bebe1f5c031ec20214385163ce246ba0e
Masterchinedum <chinedu02k@gmail.com>
2024-02-13 18:53:06 +0100

    Add hero section and footer to App.jsx and update App.css

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,16 @@ function App() {
       <div className='navCont'>
         <Navbar />
       </div>
-      <div className='heroO'>
-        <div className = 'hero'>
-          <h1>Vanlife</h1>
-          <p>Explore the world in a van</p>
+      <div className = 'hero'>
+        <h1 className='heroHeading'>You got the travel plans, we got the travel vans.</h1>
+        <p className='heroText'>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+        <div className = 'heroBtnc'>
+          <button className='heroBtn center'>Find your van</button>
         </div>
       </div>
+      <footer>
+        <p>© 2024 VanLife</p>
+      </footer>
 
     </>
   )
