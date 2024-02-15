d6ce38d28211b44d6323819a564fd6b2386d5781
Masterchinedum <chinedu02k@gmail.com>
2024-02-15 08:37:12 +0100

    Update App.css and Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from '../assets/vanlife.svg';
 import './Navbar.css'; // Import your CSS file for styling
 
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/:catchAll(.*)" element={<p>Not found</p>}></Route>
+    </Routes>
+
+  </Router>,
+)
