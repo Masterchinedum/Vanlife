f31d4563ae1b4888d14f05540cf05e766572910c
Masterchinedum <chinedu02k@gmail.com>
2024-02-17 06:25:58 +0100

    Add About page and update styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx';
+import About from './pages/About.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <Routes>
       <Route path="/" element={<App />}></Route>
-      <Route path="/about" element={<p>About</p>}></Route>
+      <Route path="/about" element={<About />}></Route>
       <Route path="/:catchAll(.*)" element={<p>Not found</p>}></Route>
     </Routes>
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,16 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer.jsx';
-// import AboutMe from './pages/AboutMe';
-import '../App.css';
+
+import './About.css';
 
 function App() {
   return (
     <>
-      <div className='navCont'>
+      <div className='aboutPage'>
         <Navbar />
       </div>
-      <div className = 'hero'>
-        <h1 className='heroHeading'>You got the travel plans, we got the travel vans.</h1>
-        <p className='heroText'>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-        <div className = 'heroBtnc'>
-          <button className='heroBtn center'>Find your van</button>
-        </div>
-      </div>
+        <div className='aboutHero'></div>
 
       <Footer />
 
