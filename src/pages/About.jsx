a184684e710fe45fdc00e2159548067769ea85e9
Masterchinedum <chinedu02k@gmail.com>
2024-02-17 08:45:17 +0100

    Update heading level in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,10 +21,10 @@ function App() {
             Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.
             </p>
             <div className='aboutCTA'> 
-                <h1>
+                <h2>
                     Your destination is waiting. <br></br>
                     Your van is ready.
-                </h1>
+                </h2>
                 <button>Learn More</button>
             </div>
         </div>
