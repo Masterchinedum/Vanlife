82acfe24f76b8c98d2fcbd0cae4cebb5e5460f33
Masterchinedum <chinedu02k@gmail.com>
2024-02-17 08:52:21 +0100

    Fix links in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import logo from '../assets/vanlife.svg';
 import './Navbar.css'; // Import your CSS file for styling
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__container">
         <img src={logo} alt="Vanlife Logo" className="navbar__logo" />
         <ul className="navbar__menu">
-          <li className="navbar__item"><a href="./" className="navbar__link">Home</a></li>
-          <li className="navbar__item"><a href="./about" className="navbar__link">About</a></li>
+          <li className="navbar__item"><a Link="./" className="navbar__link">Home</a></li>
+          <li className="navbar__item"><a Link="./about" className="navbar__link">About</a></li>
           <li className="navbar__item"><a href="#" className="navbar__link">Services</a></li>
           <li className="navbar__item"><a href="#" className="navbar__link">Contact</a></li>
         </ul>
