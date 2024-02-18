1ee5a4fa87e0164aa548945ce5b8323bbd84d0c3
Masterchinedum <chinedu02k@gmail.com>
2024-02-18 13:50:53 +0100

    Update Vans.jsx with more descriptive text

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -2,7 +2,11 @@ import './Vans.css';
 
 const Vans = () => {
   return (
-    <div>Vans for sale here</div>
+    <div>
+      <p>
+      Vans for sale here! Check out our collection of classic and modern sneakers from the world's most popular streetwear and skate brands.
+      </p>
+      </div>
   )
 }
 
