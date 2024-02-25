7a67aafc6bf82bafa9a25de9d8b463740195b81a
Masterchinedum <chinedu02k@gmail.com>
2024-02-25 08:39:53 +0100

    Update dependencies and remove commented code in Vans.jsx

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -2,22 +2,16 @@ import React, { useState, useEffect } from "react";
 // import Van from "../components/Van";
 
 export default function Vans() {
-  // const [vans, setVans] = useState([]);
-
+  const [vans, setVans] = useState([])
   useEffect(() => {
-    fetch("/api/vans")
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-  }, []);
-
+      fetch("/api/vans")
+          .then(res => res.json())
+          .then(data => setVans(data.vans))
+  }, [])
+  
+  console.log(vans)
+  
   return (
-    <div>
-      <h1>Vans Available for Rent</h1>
-      {/* <div className="vans-container">
-        {vans.map((van) => (
-          <Van key={van.id} van={van} />
-        ))}
-      </div> */}
-    </div>
-  );
+      <h1>Vans page goes here 🚐</h1>
+  )
 }
