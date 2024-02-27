b6e1dae69c761089ac4fb42c75658c52b9060a84
Masterchinedum <chinedu02k@gmail.com>
2024-02-27 03:55:36 +0100

    Add vansDetail page to routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import About from './pages/About.jsx';
 import Vans from './pages/Vans.jsx';
+import vansDetail from './pages/vansDetail.jsx';
 import Navbar from './components/Navbar.jsx';
-import Footer from "./components/Footer.jsx"
+import Footer from "./components/Footer.jsx";
 import './index.css';
 import "./server"; // Connect to the server for API calls in development
 
@@ -18,6 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path="/" element={<App />}></Route>
       <Route path="/about" element={<About />}></Route>
       <Route path="/vans" element={<Vans /> }> </Route>
+      <Route path="/vans/:id" element={<vansDetail />} />
       {/* <Route path="*" element={<Navigate replace to="/404" />} /> */}
     </Routes>
     <footer> 
---
+++ src/pages/vansDetail.jsx
@@ -0,0 +1,12 @@
+
+
+const vansDetail = () => {
+    return (
+        <div>
+            <h1>Vans Details Goes Here</h1>
+            {/* Add your content here */}
+        </div>
+    );
+}
+
+export default vansDetail;
