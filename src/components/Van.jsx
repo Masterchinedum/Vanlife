69d677d620e23dcf00dc962c4f10e360a540a2c1
Masterchinedum <chinedu02k@gmail.com>
2024-02-29 06:50:07 +0100

    Refactor Van component to use react-router-dom

--- src/components/Van.jsx
+++ src/components/Van.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import { Link } from 'react-router-dom';
 import './Van.css';
 
 export default function Van({ van }) {
   return (
-    <div className="van" key ={van.id}>
-      <img src={van.imageUrl} />
-      <div className="van-info">
-          <h3>{van.name}</h3>
-          <p>${van.price}<span>/day</span></p>
+    <Link to = "/vans/${van.id}">
+      <div  className="van" key ={van.id}>
+        <img src={van.imageUrl} />
+        <div className="van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}<span>/day</span></p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
       </div>
-      <i className={`van-type ${van.type} selected`}>{van.type}</i>
-    </div>
+    </Link>
   );
 }
