ca379cf1f611201b12eb114467c54ec56d04ed32
Masterchinedum <chinedu02k@gmail.com>
2024-03-03 08:30:36 +0100

    Add API call to fetch van details

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -3,6 +3,15 @@ import { useParams } from "react-router-dom";
 function VanDetail() {
   const params = useParams();
   console.log(params);
+
+  useEffect(() => {
+    fetch("/vans/:id")
+      .then((response) => response.json())
+      .then((data) => setVans(data.vans))
+      .catch((error) => console.error("Error fetching vans:", error));
+  }, []);
+
+
   return (
     <div>
         <h1>Van Detail Page Goes Here </h1>
