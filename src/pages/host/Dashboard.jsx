a082fe4bb926c3ba72e6e17752be2015a0150654
Masterchinedum <chinedu02k@gmail.com>
2024-03-07 23:10:58 +0100

    editting the dashboard

--- src/pages/host/Dashboard.jsx
+++ src/pages/host/Dashboard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom';
+import "./Dashboard.css";
+import Sidebar from './Sidebar';
 
 const Dashboard = () => {
   return (
     <div>
-        <p>Dashboard</p>
+        <div>
+            <Sidebar />
+        </div>
+        <div className="dashboard">
+            <h1>Dashboard</h1>
+            <Outlet />
+        </div>
     </div>
   )
 }
