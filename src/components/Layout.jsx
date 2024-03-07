c1e221a8b3782b33bc05987a07a8133cec2270d0
Masterchinedum <chinedu02k@gmail.com>
2024-03-07 22:24:13 +0100

    Remove unnecessary comment and update sidebar width

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Navbar";
 import { Outlet } from 'react-router-dom';
 import Footer from "./Footer";
+import Sidebar from "../pages/host/Sidebar";
 
 const Layout = () => {
   return (
     <div>
         <Navbar />
+        <Sidebar />
         <Outlet />
         <Footer />
     </div>
