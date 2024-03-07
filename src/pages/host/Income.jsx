21dfd77a63df7ffba55f8df9b517ed769fcf8971
Masterchinedum <chinedu02k@gmail.com>
2024-03-07 07:40:12 +0100

    Add income data and styling to Income component

--- src/pages/host/Income.jsx
+++ src/pages/host/Income.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import "./income.css";
 
 const Income = () => {
-  return (
-    <div>
-        <p>Income</p>
-    </div>
-  )
-}
+  const incomeData = [
+    { month: 'January', income: '$10,000' },
+    { month: 'February', income: '$12,500' },
+    { month: 'March', income: '$15,200' },
+  ];
 
-export default Income
+  return (
+    <div className="income-container">
+      <h2>Income Overview</h2>
+      <div className="income-chart">
+        {/* Replace with your preferred charting library */}
+        <p>Chart will be displayed here</p>
+      </div>
+      <ul className="income-list">
+        {incomeData.map((item) => (
+          <li key={item.month}>
+            <span className="month">{item.month}</span>
+            <span className="amount">{item.income}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Income;
