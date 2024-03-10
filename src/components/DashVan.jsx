4d0111d3d97d85c71ba6a24203913f09f0ec26c4
Masterchinedum <chinedu02k@gmail.com>
2024-03-10 22:27:22 +0100

    Add link to view details in DashVan component

--- src/components/DashVan.jsx
+++ src/components/DashVan.jsx
@@ -1,21 +1,28 @@
 // DashVan.jsx
+import { Link } from 'react-router-dom';
 import './DashVan.css';
 
 function DashVan({ van }) {
   return (
     <div className="dash-van" key={van.id}>
-      <img src={van.imageUrl} alt={`Image of ${van.name}`} />
-      <div className="van-info">
-        <div className="van-header">
-          <h3>{van.name}</h3>
-          <p>${van.price}<span>/day</span></p>
+      <Link to = {`/host/vans/${van.id}`}
+        aria-label={`View details for ${van.name}, 
+        priced at $${van.price} per day`}
+        >
+        <img src={van.imageUrl} alt={`Image of ${van.name}`} />
+        <div className="van-info">
+          <div className="van-header">
+            <h3>{van.name}</h3>
+            <p>${van.price}<span>/day</span></p>
+          </div>
+          <div className="van-specs">
+            <span className={`van-type ${van.type.toLowerCase()}`}>{van.type}</span>
+          </div>
         </div>
-        <div className="van-specs">
-          <span className={`van-type ${van.type.toLowerCase()}`}>{van.type}</span>
-        </div>
-      </div>
+      </Link>
+
     </div>
   );
-}
+};
 
-export default DashVan;
+export default DashVan
