24c7a66444a99aed9fa6cfaabcfdda4e1e98314d
Masterchinedum <chinedu02k@gmail.com>
2024-03-10 21:33:31 +0100

    Add styling and information to DashVan component

--- src/components/DashVan.jsx
+++ src/components/DashVan.jsx
@@ -7,10 +7,14 @@ function DashVan({ van }) {
       <div  className="Dashvan" key ={van.id}>
           <img src={van.imageUrl} alt={`Image of ${van.name} ` } />
           <div className="van-info">
+            <div className = 'van-header'>
               <h3>{van.name}</h3>
               <p>${van.price}<span>/day</span></p>
+            </div>
+            <div className='van-specs'>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </div>
           </div>
-          <i className={`van-type ${van.type} selected`}>{van.type}</i>
       </div>
 
   );
