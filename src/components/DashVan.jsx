6d7aa7d2a09df046d427d089215dde07411aef90
Masterchinedum <chinedu02k@gmail.com>
2024-03-10 21:47:24 +0100

    Refactor DashVan component and CSS

--- src/components/DashVan.jsx
+++ src/components/DashVan.jsx
@@ -1,23 +1,21 @@
+// DashVan.jsx
 import './DashVan.css';
-
 
 function DashVan({ van }) {
   return (
-
-      <div  className="Dashvan" key ={van.id}>
-          <img src={van.imageUrl} alt={`Image of ${van.name} ` } />
-          <div className="van-info">
-            <div className = 'van-header'>
-              <h3>{van.name}</h3>
-              <p>${van.price}<span>/day</span></p>
-            </div>
-            <div className='van-specs'>
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
-            </div>
-          </div>
+    <div className="dash-van" key={van.id}>
+      <img src={van.imageUrl} alt={`Image of ${van.name}`} />
+      <div className="van-info">
+        <div className="van-header">
+          <h3>{van.name}</h3>
+          <p>${van.price}<span>/day</span></p>
+        </div>
+        <div className="van-specs">
+          <span className={`van-type ${van.type.toLowerCase()}`}>{van.type}</span>
+        </div>
       </div>
-
+    </div>
   );
 }
 
-export default DashVan
+export default DashVan;
