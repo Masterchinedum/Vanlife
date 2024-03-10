930e880064e85d9b5c6f956969c567f29f528bab
Masterchinedum <chinedu02k@gmail.com>
2024-03-10 20:55:02 +0100

    Add state and useEffect hook to fetch and display hosted vans

--- src/pages/host/DashVans.jsx
+++ src/pages/host/DashVans.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import DashVan from '../../components/DashVan';
 
 const DashVans = () => {
+  const [vans, setVans] = useState([]);
+  useEffect(() => {
+    fetch("/api/host/vans")
+      .then((response) => response.json())
+      .then((data) => setVans(data.vans))
+      .catch((error) => console.error("Error fetching vans:", error));
+  }, []);
+
+  
   return (
     <div>
-        List of users's Vans Goes Here
+      <h1>Vans You're Hosting</h1>
+      <div>
+        {vans.map((van) => (
+          <DashVan key={van.id} van={van} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
-
-export default DashVans
+export default DashVans;
