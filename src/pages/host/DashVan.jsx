ab5fa51df750680facc37298b7748b686aae003c
Masterchinedum <chinedu02k@gmail.com>
2024-03-10 23:15:40 +0100

    Refactor DashVan component styles

--- src/pages/host/DashVan.jsx
+++ src/pages/host/DashVan.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { useParams } from "react-router-dom";
+import './DashVandet.css';
 
 const DashVan = () => {
   const params = useParams();
