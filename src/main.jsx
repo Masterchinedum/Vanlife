1110566d8579c1b7585b96ebad967583e981021a
Masterchinedum <chinedu02k@gmail.com>
2024-03-10 15:07:44 +0100

    Update routes and sidebar styling

--- src/main.jsx
+++ src/main.jsx
@@ -25,6 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route index element={<Dashboard /> } />
           <Route path="income" element={<Income />} />
           <Route path="reviews" element = {<Reviews />} />
+          
         </Route>
         <Route path="about" element={<About />} />
         <Route path="vans" element={<Vans />} />
