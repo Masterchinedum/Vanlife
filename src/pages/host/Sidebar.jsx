bb5f9a76b7d4ec370d06a0220f88913cf4e5de2a
Masterchinedum <chinedu02k@gmail.com>
2024-03-10 06:33:00 +0100

    Add styling to active and other links in Sidebar

--- src/pages/host/Sidebar.jsx
+++ src/pages/host/Sidebar.jsx
@@ -1,14 +1,21 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import "./Sidebar.css";
+
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <h3>Dashboard</h3>
       <ul>
-        <li><Link to="/host">Me</Link></li>
-        <li><Link to="/host/income">Income</Link></li>
-        <li><Link to="/host/reviews">Reviews</Link></li>
+        <li><NavLink to="/host"
+        className={({isActive}) => isActive ? "activeLink" : "otherLink"}>
+        Me</NavLink></li>
+        <li><NavLink to="/host/income"
+        className={({isActive}) => isActive ? "activeLink" : "otherLink"}>
+        Income</NavLink></li>
+        <li><NavLink to="/host/reviews"
+        className={({isActive}) => isActive ? "activeLink" : "otherLink"}>
+        Reviews</NavLink></li>
       </ul>
     </div>
   );
