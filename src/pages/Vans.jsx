c2497abd8ae52792d7164020dfb6545a0f350d15
Masterchinedum <chinedu02k@gmail.com>
2024-04-11 19:01:31 +0100

    Refactor Vans component to filter vans based on queryParam and improve data fetching logic

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -5,27 +5,26 @@ import "./Vans.css";
 
 function Vans() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const queryParam = searchParams.get("type")
+  const queryParam = searchParams.get("type");
   const [vans, setVans] = useState([]);
 
-  useEffect(() => {
-    if (queryParam) {
-      const filteredVans = vans.filter((van) => van.type === queryParam);
-      setVans(filteredVans);
-    }
-  }, [queryParam, vans]);
-  
-  const fetchVansData = async () => {
+  const fetchVansData = async (type) => {
     try {
       const storedVans = localStorage.getItem("vans");
-
       if (storedVans) {
-        setVans(JSON.parse(storedVans));
+        let filteredVans = JSON.parse(storedVans);
+        if (type) {
+          filteredVans = filteredVans.filter((van) => van.type === type);
+        }
+        setVans(filteredVans);
       } else {
         const response = await fetch("/api/vans");
         const data = await response.json();
-
-        setVans(data.vans);
+        let fetchedVans = data.vans;
+        if (type) {
+          fetchedVans = fetchedVans.filter((van) => van.type === type);
+        }
+        setVans(fetchedVans);
         localStorage.setItem("vans", JSON.stringify(data.vans));
       }
     } catch (error) {
@@ -34,12 +33,26 @@ function Vans() {
   };
 
   useEffect(() => {
+    fetchVansData(queryParam);
+  }, [queryParam]);
+
+  useEffect(() => {
     fetchVansData();
   }, []);
 
+  const handleFilter = (type) => {
+    setSearchParams({ type });
+  };
+
   return (
     <div className="container">
-      <h1 className="vansAvail">Vans Available for Rent</h1>
+      <h1 className="vans-avail">Vans Available for Rent</h1>
+      <div className="filter-buttons">
+        <button onClick={() => handleFilter("")}>All</button>
+        <button onClick={() => handleFilter("type1")}>Type 1</button>
+        <button onClick={() => handleFilter("type2")}>Type 2</button>
+        <button onClick={() => handleFilter("type3")}>Type 3</button>
+      </div>
       <div className="vans-container">
         {vans.map((van) => (
           <Van key={van.id} van={van} />
